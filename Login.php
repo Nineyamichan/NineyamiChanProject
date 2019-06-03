b61fced30a8240035431eca2ab6dad85a09de340
Nineyamichan <50326575+Nineyamichan@users.noreply.github.com>
2019-06-02 22:23:46 -0300

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,94 @@
+<html>
+    <head>
+        
+        <meta charset="UTF-8">
+ 
+<link rel="shortcut icon" HREF="https://vignette.wikia.nocookie.net/simswiki/images/f/f8/Vampiro_%28TS4%29.png/revision/latest/scale-to-width-down/40?cb=20170422015340&path-prefix=pt-br">  
+     <title> Sistema de Login- Yami </title>
+   
+ </head>
+<!-- Wikplayer https://www.wikplayer.com -->
+<script type="text/javascript" src="https://www.wikplayer.com/code.js" 
+data-config="{'skin':'skins/wikfull/funkyRed/skin.css','volume':100,'autoplay':false,'shuffle':false,'repeat':0,'showcomment':false,'marqueetexton':true,'placement':'top','showplaylist':false,'playlist':[{'title':'Blutengel%20-%20Morningstar','url':'https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DGKKe3C0D70U'},{'title':'Blutengel%20-%20Reich%20mir%20die%20Hand%20','url':'https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DNldXlXY5hGY'},{'title':'Blutengel%20-%20%C3%9Cber%20den%20Horizont','url':'https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DKquTFWUyboA'},{'title':'Terminal%20Choice%20-%20Don%27t%20Go','url':'https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DrvTDXjPA_bw'}]}" ></script>
+<!-- Wikplayer code end -->
+
+<style type="text/css">* {cursor: url(http://ani.cursors-4u.net/symbols/sym-9/sym828.ani), url(http://ani.cursors-4u.net/symbols/sym-9/sym828.png), auto !important;}</style><a href="http://www.cursors-4u.com/cursor/2018/02/17/dancing-red-rose.html" target="_blank" title="Dancing Red Rose"><img src="http://cur.cursors-4u.net/cursor.png" border="0" alt="Dancing Red Rose" style="position:absolute; top: 0px; right: 0px;" /></a>
+
+
+
+ 
+                <br>
+                <br>
+                <br>
+                <br>
+ <center> <img src="http://gibsy.g.i.pic.centerblog.net/2525237whh10dl9sw.gif" alt="some text" width=350 height=250> </center>
+ 
+  <body background="https://images2.alphacoders.com/881/881377.jpg"   >
+        
+     
+<form action="CADASTRO-BD.php" method="POST"> 
+            <center>
+                
+                <table  BACKGROUND="https://railroadphotosbypanos.net/wp-content/uploads/2018/10/nature-sketches-black-white-laptop-wallpapers-wallpaper-for-desktop-laptop-ms96-space-astronomy-galaxy-dark.jpg" border="2">
+
+                
+    <th colspan="2">  Login  - Seja bem vindo! </th> 
+
+                     
+                       <tr>            
+                    <td bgcolor="#BDBDBD"> E-mail:  </td>
+                    <td><input type="text" name="email" placeholder="Informe seu e-mail, por favor" size="60"  required=""> </td> <br> 
+                 
+ </tr>
+   <tr> 
+                    <td bgcolor="#BDBDBD"> Senha:  </td>
+                    <td> <input type="password" name="senha" placeholder="Informe a senha, por favor"  size="60"  required="" accept="" >
+                    </td> <br> 
+                       </tr>
+                    <tr>
+                        <td align = "center" colspan="2"> 
+                        <INPUT TYPE="SUBMIT" VALUE="Enviar">
+                        <INPUT TYPE="RESET" VALUE="Limpar"></td> 
+                    </tr>  
+     <tr>  <td>            
+ <p>    Não está cadastrado? <a href=" http://localhost/PhpProject2/index.php" > Cadastre-se </a>  </p>  </td> </tr>
+                </table>
+            </center>
+        </form> 
+         
+     
+   <form action="index2" method="POST"> 
+      
+            <center>
+                
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                
+                
+                <table  BACKGROUND="https://i.pinimg.com/originals/6a/5a/eb/6a5aeb7b6f401e6b6d5ba4d50d91021e.png" border="2">
+
+     
+                    
+                    
+                </table>
+            </center>
+       
+       
+       
+       
+       
+       
+        </form> 
+        
+        </section>
+    </div>    
+</body>
+ 
+    
+    
+</html>
