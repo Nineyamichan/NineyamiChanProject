b779e7781d4c3b687402681f702d43cbf63adbcc
Nineyamichan <50326575+Nineyamichan@users.noreply.github.com>
2019-06-03 02:34:20 -0300

    Update ban.php

--- ban.php
+++ ban.php
@@ -1,14 +1,13 @@
 <?php
- $nome_servidor = "localhost";
- $nome_usuario = "root";
- $senha = "";
- $banco = "Cadastro";
+ $hostname = "localhost";
+ $user = "root";
+ $password = "";
+ $database = "cadastro";
          
- $conecta = new mysqli($nome_servidor, $nome_usuario, $senha,$banco);
+ $conecta = mysqli_connect($hostname,$user,$password,$database);
  if ($conecta->connect_error) {
  die("Conexão falhou: " . $conecta->connect_error."<br>");
  }
  echo "Conexão realizada com sucesso";
  $conecta->close();
 ?>
-
